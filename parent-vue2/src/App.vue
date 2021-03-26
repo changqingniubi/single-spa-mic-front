b25bc984363dbb33dc7cfaef151e93c92916bc15
changqing <qing.chang@eeoa.com>
2021-03-26 11:25:10 +0800

    first commit

---
+++ parent-vue2/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+     <router-link to="/">Home</router-link> |
+     <router-link to="/about">About</router-link>
+     <router-link to="/vue">加载vue应用</router-link>
+      <router-view/>
+     <!--子应用加载的位置-->
+     <div id="vue"></div>
+  </div>
+</template>
+
+<style>
+
+</style>
